2584d3d42fe0e316f0b30590b17f4aa265c78c91
Dao Pham <daopm.dean@gmail.com>
2022-05-08 23:18:38 +0700

    list movies

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import ReactDOM from 'react-dom/client'; this shit does not work
+// import ReactDOM from 'react-dom/client'; this sh!t does not work
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
