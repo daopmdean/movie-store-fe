3313cf0ea6cb2f42d5c79117f10ceb2ac20e2ac3
Dao Pham <daopm.dean@gmail.com>
2022-05-09 22:04:01 +0700

    Catgories

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component, Fragment } from 'react';
+import { useRouteMatch } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Admin from './components/Admin';
+import Categories from './components/Categories';
 import Home from './components/Home';
 import Movies from './components/Movies';
 
@@ -27,6 +29,9 @@ export default function App() {
               <li className='list-group-item'>
                 <Link to='/movies'>Movies</Link>
               </li>
+              <li className='list-group-item'>
+                <Link to='/by-category'>Categories</Link>
+              </li>
               <li className='list-group-item  '>
                 <Link to='/admin'>Manage Catalogue</Link>
               </li>
@@ -36,12 +41,26 @@ export default function App() {
 
         <div className='col-md-10'>
           <Switch>
-          <Route path='/movies/:id/:ds'>
+          <Route path='/movies/:id'>
               <Movie />
             </Route>
             <Route path='/movies'>
               <Movies />
             </Route>
+
+            <Route path='/by-category'>
+              <CategoryPage />
+            </Route>
+
+            <Route exact path='/by-category/drama' 
+            render={(props) => <Categories {...props} title={`Drama`}/>}
+            />
+
+            <Route exact path='/by-category/comedy' 
+            render={(props) => <Categories {...props} title={`Comedy`}/>}
+            />
+
+
             <Route path='/admin'>
               <Admin />
             </Route>
@@ -58,6 +77,19 @@ function Movie() {
   let {id} = useParams();
   return(
     <h2>Movie {id}</h2>
+  );
+}
+
+function CategoryPage() {
+  let {path, url} = useRouteMatch();
+  return(
+    <div>
+      <h2>Categories</h2>
+      <ul>
+        <li><Link to={`${path}/comedy`}>Comedy</Link></li>
+        <li><Link to={`${url}/drama`}>Drama</Link></li>
+      </ul>
+    </div>
   );
 }
 
---
+++ src/components/Categories.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Categories extends Component {
+  render() {
+    return <h2>Category {this.props.title}</h2>
+  }
+}
+
+export default Categories;
