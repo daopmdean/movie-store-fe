88ba047cea4b8db6d5bd1faf68670b5394e7d486
Dao Pham <daopm.dean@gmail.com>
2022-05-14 17:05:44 +0700

    get one movie

--- src/components/OneMovie.js
+++ src/components/OneMovie.js
@@ -1,24 +1,77 @@
 import React, { Component, Fragment } from "react";
 
 export default class OneMovie extends Component {
-  state = { movie: {} };
+  state = {
+    movie: {},
+    isLoaded: false,
+    error: null,
+  };
 
   componentDidMount() {
-    this.setState({
-      movie: {
-        id: this.props.match.params.id,
-        title: "Some movie",
-        runtime: 150,
-      },
-    });
+    fetch("http://localhost:4000/v1/movies/" + this.props.match.params.id)
+      .then((res) => {
+        console.log("Response code: " + res.status);
+
+        if (res.status !== 200) {
+          let err = Error;
+          err.message = "Invalid response code: " + res.status;
+          this.setState({
+            error: err,
+          });
+        }
+        return res.json();
+      })
+      .then((json) => {
+        this.setState(
+          {
+            movie: json.movie,
+            isLoaded: true,
+          },
+          (error) => {
+            this.setState({
+              isLoaded: true,
+              error,
+            });
+          }
+        );
+      });
   }
 
   render() {
+    const { movie, isLoaded, error } = this.state;
+
+    if (error) {
+      return <p>Error: {error.message}</p>;
+    }
+
+    if (!isLoaded) {
+      return <p>Loading...</p>;
+    }
+
+    if (movie.genres) {
+      movie.genres = Object.values(movie.genres);
+    } else {
+      movie.genres = [];
+    }
+
     return (
       <Fragment>
         <h2>
-          Movie: {this.state.movie.title} {this.state.movie.id}
+          Movie: {movie.title} ({movie.year})
         </h2>
+
+        <div className="float-start">
+          <small>Rating: {movie.mpaaRating}</small>
+        </div>
+        <div className="float-end">
+          {movie.genres.map((m, index) => (
+            <span className="badge bg-secondary me-1" key={index}>
+              {m}
+            </span>
+          ))}
+        </div>
+        <div className="clearfix"></div>
+        <hr />
 
         <table className="table table-compact table-striped">
           <thead></thead>
@@ -27,13 +80,19 @@ export default class OneMovie extends Component {
               <td>
                 <strong>Title:</strong>
               </td>
-              <td>{this.state.movie.title}</td>
+              <td>{movie.title}</td>
+            </tr>
+            <tr>
+              <td>
+                <strong>Description:</strong>
+              </td>
+              <td>{movie.description}</td>
             </tr>
             <tr>
               <td>
                 <strong>Run time:</strong>
               </td>
-              <td>{this.state.movie.runtime} minutes</td>
+              <td>{movie.runtime} minutes</td>
             </tr>
           </tbody>
         </table>
