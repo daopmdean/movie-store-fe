73ec1bb3309fd7b007c06d82238eca3ad5a74554
Dao Pham <daopm.dean@gmail.com>
2022-05-15 22:53:24 +0700

    categories page

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,8 +1,59 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Fragment } from "react/cjs/react.production.min";
 
 class Categories extends Component {
+  state = {
+    genres: [],
+    isLoaded: false,
+    error: null,
+  };
+
+  componentDidMount() {
+    fetch("http://localhost:4000/v1/genres")
+      .then((res) => {
+        console.log("Response code: " + res.status);
+
+        if (res.status !== 200) {
+          let err = Error;
+          err.message = "Invalid response code: " + res.status;
+          this.setState({
+            error: err,
+          });
+        }
+        return res.json();
+      })
+      .then((json) => {
+        this.setState(
+          {
+            genres: json.genres,
+            isLoaded: true,
+          },
+          (error) => {
+            this.setState({
+              isLoaded: true,
+              error,
+            });
+          }
+        );
+      });
+  }
+
   render() {
-    return <h2>Category {this.props.title}</h2>;
+    const { genres, isLoaded, error } = this.state;
+
+    return (
+      <Fragment>
+        <h2>Categories {this.props.title}</h2>
+        <ul>
+          {genres.map((g) => (
+            <li key={g.id}>
+              <Link to={`/genres/${g.id}`}>{g.genreName}</Link>
+            </li>
+          ))}
+        </ul>
+      </Fragment>
+    );
   }
 }
 
