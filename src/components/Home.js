175d002dbeae2f67f2a182d5aaea3048da1c0ea5
Dao Pham <daopm.dean@gmail.com>
2022-05-19 21:14:28 +0700

    show ticket at home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,14 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
+import Ticket from "./../images/movie_tickets.jpg";
 
 class Home extends Component {
   render() {
     return (
-      <h2>Home</h2>
+      <div className="text-center">
+        <h2>Home</h2>
+        <hr />
+        <img src={Ticket} alt="Movie Ticket" />
+      </div>
     );
   }
 }
